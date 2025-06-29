b33e7e4ca83490ac8c72329783bdc5afa42065d9
aazim <aazim@tgcl.co.nz>
2025-06-30 11:41:40 +1200

    Updated setup

--- setup.py
+++ setup.py
@@ -2,21 +2,29 @@
 from setuptools import setup, find_packages
 import os
 
+
 # Function to read the requirements.txt file
 def read_requirements():
     with open(os.path.join('clients', 'python', 'requirements.txt')) as req:
         return req.read().splitlines()
 
+
 setup(
     name='kindo-message-client',
-    version='0.1.0',  # Start with an initial version
-    author='kingsleywang1984', # Or your name/organization
+    version='0.1.1',  # Good practice to increment the version after a fix
+    author='kingsleywang1984',
     description='A Python client to send messages to an SQS queue.',
-    packages=find_packages(where='clients/python'), # Tells setuptools to look for packages in this sub-directory
-    package_dir={'': 'clients/python'}, # Specifies that the root package directory is 'clients/python'
 
-    # This tells pip what other packages your module depends on.
-    # It will automatically install them.
+    # --- THIS IS THE CRITICAL FIX ---
+    # Use 'py_modules' to explicitly list standalone .py files to include.
+    py_modules=['producer', 'signer', 'config'],
+
+    # You can still use find_packages if you have actual packages (like 'tests')
+    packages=find_packages(where='clients/python'),
+
+    # This tells setuptools that the root for the modules and packages is 'clients/python'
+    package_dir={'': 'clients/python'},
+
     install_requires=read_requirements(),
 
     classifiers=[
