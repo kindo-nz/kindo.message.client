a355bfcf9a02167df3710131b138eea0fb2b2937
aazim <aazim@tgcl.co.nz>
2025-06-30 16:31:30 +1200

    Matched the schema to include security level

--- clients/python/kindo_message_client/producer.py
+++ clients/python/kindo_message_client/producer.py
@@ -1,23 +1,30 @@
 import json
 import os
 import jsonschema
-from typing import Literal, TypedDict
+from typing import Literal, TypedDict, NotRequired
 from .signer import signed_post
 from .config import AWS_REGION, AWS_SERVICE, SCHEMA_PATH
+
 
 class ProducerPayload(TypedDict):
     event_type: str
     message_channel: str
     behavior: Literal["instant"]
     payload: dict
+    security_level: NotRequired[Literal["sensitive", "normal"]]
+
 
 def send_to_producer(
-    url: str,
-    message: ProducerPayload,
-    region: str = AWS_REGION,
-    service: str = AWS_SERVICE,
-    schema_path: str = SCHEMA_PATH
+        url: str,
+        message: ProducerPayload,
+        region: str = AWS_REGION,
+        service: str = AWS_SERVICE,
+        schema_path: str = SCHEMA_PATH
 ) -> str:
+    # Set default security_level if not provided
+    if "security_level" not in message:
+        message["security_level"] = "normal"
+
     with open(os.path.join(os.path.dirname(__file__), schema_path)) as f:
         schema = json.load(f)
     try:
