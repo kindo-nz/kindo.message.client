8e950c30e03f3f9efad174497b7006ec50e7ccee
Yao Hong Chun <yaohong@tgcl.co.nz>
2025-07-22 09:12:29 +1200

    Remove required env and provide defaults instead

--- clients/python/config.py
+++ clients/python/config.py
@@ -7,16 +7,16 @@ class ConfigError(Exception):
     pass
 
 REQUIRED_ENV_VARS = [
-    "KINDO_MESSAGE_PRODUCER_URL",
-    "KINDO_MESSAGE_BEHAVIOR"
+    # "KINDO_MESSAGE_PRODUCER_URL",
+    # "KINDO_MESSAGE_BEHAVIOR"
 ]
 
 for var in REQUIRED_ENV_VARS:
     if var not in os.environ:
         raise ConfigError(f"Missing required environment variable: {var}")
 
-KINDO_MESSAGE_PRODUCER_URL  = os.environ["KINDO_MESSAGE_PRODUCER_URL"]
-KINDO_MESSAGE_BEHAVIOR      = os.environ["KINDO_MESSAGE_BEHAVIOR"]
+KINDO_MESSAGE_PRODUCER_URL  = os.environ["KINDO_MESSAGE_PRODUCER_URL"] or ""
+KINDO_MESSAGE_BEHAVIOR      = os.environ["KINDO_MESSAGE_BEHAVIOR"] or "instant"
 
 AWS_REGION                  = os.environ.get("AWS_REGION", "ap-southeast-2")
 AWS_SERVICE                 = os.environ.get("AWS_SERVICE", "lambda")
